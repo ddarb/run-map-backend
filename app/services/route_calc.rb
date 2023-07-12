1af010dba495da35d968b352fbecabc3f00fc600
Daniel Byrne <danielbyrne78@gmail.com>
2023-07-12 22:52:09 +1000

    custom param validators

--- app/services/route_calc.rb
+++ app/services/route_calc.rb
@@ -5,9 +5,39 @@ class RouteCalc
     attr_accessor :from, :to
 
     validates :from, :to, presence: true
-    # This validator is wrong, we want the from and to to be of the form from='<float>,<float>' and to = '<float>,<float>' 
-    validates :from, :to, numericality: true
+    
+    # Custom validations for formatting and type
+    validate :lat_long_comma_sep
+    validate :lat_long_numbers
 
+    def lat_long_numbers
+        for param in [from, to] do
+            lnglat = param.split(',')
+            debugger
+            for num in lnglat do
+                if !is_number?(num)
+                    errors.add :base, "Lng or Lat param %s is not a number" % [num]
+                end
+            end
+        end
+    end
+
+    def lat_long_comma_sep
+        for param in [from, to] do
+            lnglat = param.split(',')
+            # TODO: specify which param is wrong
+            
+            if lnglat.length() != 2
+                errors.add :base, "From and to params must be of the form from=to=`<float: lng>,<float: lat>`"
+            end
+        end
+    end
+
+    
+
+    def is_number? string
+        true if Float(string) rescue false
+    end
     # Return a geojson line from a json file.
     # This will eventually be replace by a call to the graphopper API
     def perform_calc
