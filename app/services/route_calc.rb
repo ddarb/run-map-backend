c8d97ea395591353725d6d26328102a8fb55afd3
Daniel Byrne <danielbyrne78@gmail.com>
2023-10-16 22:12:06 +1100

    return the feature line

--- app/services/route_calc.rb
+++ app/services/route_calc.rb
@@ -3,17 +3,15 @@ class RouteCalc
     include ActiveModel::Validations # add validations
 
     attr_accessor :from, :to
-
     validates :from, :to, presence: true
     
     # Custom validations for formatting and type
     validate :lat_long_comma_sep
     validate :lat_long_numbers
-
+    
     def lat_long_numbers
         for param in [from, to] do
             lnglat = param.split(',')
-            debugger
             for num in lnglat do
                 if !is_number?(num)
                     errors.add :base, "Lng or Lat param %s is not a number" % [num]
@@ -26,7 +24,6 @@ class RouteCalc
         for param in [from, to] do
             lnglat = param.split(',')
             # TODO: specify which param is wrong
-            
             if lnglat.length() != 2
                 errors.add :base, "From and to params must be of the form from=to=`<float: lng>,<float: lat>`"
             end
@@ -38,6 +35,7 @@ class RouteCalc
     def is_number? string
         true if Float(string) rescue false
     end
+    
     # Return a geojson line from a json file.
     # This will eventually be replace by a call to the graphopper API
     def perform_calc
